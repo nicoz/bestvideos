9b03b2ec02fb5ede1c738c743427d8ef3916e656
Nicolas Zuasti <nicozuasti@gmail.com>
2013-08-06 16:44:24 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bestvideos::Application.config.secret_key_base = 'c539b6e6d7ca3d65e0e55ea5a9e54c353d6e854ff8570fd57f92cbf302f5a462e8178544a6f6fb243d4323f6b51262b052be4ff6f2f96f46bbb6adaddcf1883b'
